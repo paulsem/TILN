b39b99df187c32d646696b0077396b99578924e3
paulsem <32594551+paulsem@users.noreply.github.com>
2020-03-25 16:09:33 +0200

    try commit from pycharm

---
+++ GUI/interface.py
@@ -0,0 +1,12 @@
+from graphics import *
+
+
+def main():
+    win = GraphWin("My Circle", 500, 500)
+    c = Circle(Point(50, 50), 10)
+    c.draw(win)
+    win.getMouse()  # Pause to view result
+    win.close()  # Close window when done
+
+
+main()
