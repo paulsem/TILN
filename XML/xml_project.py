84f5466fa088f9cbb2aae1403db753881f84a2d6
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-03-29 13:41:41 +0300

    Add files via upload

--- XML/xml_project.py
+++ XML/xml_project.py
@@ -2,9 +2,11 @@ import xml.etree.ElementTree as ET
 import json
 import re
 
+
 with open("dic_export.jpg", "r") as fd:
     dictionar = json.load(fd)
 #print("Dictionar :  ", dictionar)
+
 
 # split in sentences
 with open('extract.txt','r') as f:
@@ -12,20 +14,13 @@ with open('extract.txt','r') as f:
     # print(content)
     sentences = re.split(r'[.?]', str(content))
     #sentences = content.split(".")
-print("Sentences :  ", sentences)
-
-# # split in words
-# with open('extract.txt','r') as f:
-#     content = f.read()
-#     # print(content)
-#     word = content.split()
-#     print("Words from sentences :  ",word)
+# print("Sentences :  ", sentences)
 
 
 xml_doc = ET.Element('TimeML')
 
 # DOCNO tag
-DOCNO = ET.SubElement(xml_doc,'DOCNO').text = '19980108'
+DOCNO = ET.SubElement(xml_doc,'DOCNO', type = 'date', temporalFunction = 'false').text = '19980108'
 
 # DOCTYPE tag
 DOCTYPE = ET.SubElement(xml_doc,'DOCTYPE',SOURCE='extract.txt').text = 'Vremuri de mult apuse'
@@ -35,14 +30,18 @@ BODY = ET.SubElement(xml_doc,'BODY')
 
 # TEXT tag
 TEXT = ET.SubElement(BODY,'TEXT')
-
+ok = 1
+# s tag (sentences)
 for i in sentences:
     s = ET.SubElement(TEXT, 's')
     for key, values in dictionar.items():
         for value in values:
             if value in i:
-                #print("Value :  ", value)
-                tag = ET.SubElement(s, key).text = value
+                # print("Value :  ", value)
+                # TIMEEX tag
+                tag = ET.SubElement(s, 'TIMEEX',id = 't' + str(ok), value = value, type = key, temporalFunction = 'true',functionInDocument = 'NONE').text = value
+                ok = ok + 1
+                # print(ok)
 
 
 
